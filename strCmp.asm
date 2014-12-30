743c75167075ce93e371987df0fb9a968da4401b
fatemeh-shaterian <fatemehshaterian@gmail.com>
2014-12-31 00:34:02 +0330

    Update strCmp.asm

--- strCmp.asm
+++ strCmp.asm
@@ -1,59 +1,51 @@
-;;;;;;;;;;;;;;;;;;;;;;;;
-; recieves:
-; address = str1
-; address_temp = str2
-; returns:
-; bx = 0 or 1		1 = true   0 = false
-
-
-;my plan
-;recived:dx,bx
-;use: si
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   recived:dx,bx               
+;;   if cx == 0  then command is like echo
+;;   use: si
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
 strCmp:
-;pusha
-mov bp,sp
-mov si, 0
-mov ax,0
-push bx
-.l1:
+	;pusha
+	mov bp,sp
+	mov si, 0
+	mov ax,0
+	push bx
+	.l1:
+		pop bx
+		mov ah,byte[bx+si]
+		cmp ah, " "
+		jne .down1
+		push bx
+		mov bx,dx
+		mov al,byte[bx+si]
+		cmp al,0
+		jne .down2
+		cmp cx,0        ;echo type
+		je .equals
+	.down1:
+		push bx
+		mov bx,dx
+		mov al,byte[bx+si]
+		.down2:
+		cmp al,ah
+		jne .notequal
 
-;pop si
-pop bx
-mov ah,byte[bx+si]
-push bx
-mov bx,dx
-mov al,byte[bx+si]
-cmp al,ah
-jne .notequal
+		cmp byte[bx+si], 0
+		je .equals
+		inc si
+        	jmp .l1
 
-cmp byte[bx+si], 0
-je .equals
-inc si
+	.equals:
+		mov bx , 1
+		;mov si,onee
+		;call printString
+		jmp .toEnd
 
-jmp .l1
-
-.equals:
-mov bx , 1
-mov si,onee
-call printString
-jmp .toEnd
-
-
-.notequal:
-mov bx , 0
-mov si, zeroo
-call printString
-
-.toEnd:
-mov sp,bp
-
-
+	.notequal:
+		mov bx , 0
+	.toEnd:
+		mov sp,bp
 ret
-
 
 address dw 0
 address_temp dw 0
-zeroo db "0",0
-onee db '1',0
