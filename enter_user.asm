9ba42335d1df77d88c8f0cb299061f956637ccd6
fatemeh-shaterian <fatemehshaterian@gmail.com>
2014-12-31 09:15:41 +0330

    Update enter_user.asm

--- enter_user.asm
+++ enter_user.asm
@@ -18,33 +18,51 @@ mov di,pass_temp;
 mov bx , pass_temp
 call getString 
 
-mov bx,user
+mov bx,user1
 mov dx,user_temp 
 call strCmp
 mov cx, bx
+cmp bx,1
+je .toPass1
 
+mov bx,user2
+mov dx,user_temp 
+call strCmp
+mov cx, bx
+cmp bx,1
+je .toPass2
+
+
+.toPass1:
 mov si,message2
 call printString
-
-mov bx,pass
+mov bx,pass1
 mov dx,pass_temp
 call strCmp
+jmp .toend
 
-;mov si,message2
-;call printString
+.toPass2:
+mov si,message2
+call printString
+mov bx,pass2
+mov dx,pass_temp
+call strCmp
+jmp .toend
 
+.toend:
 add bx , cx               
 cmp bx,2
 jne top
-
 popa
 
 ret
 ;%INCLUDE "console_io.asm"
 %INCLUDE"strCmp.asm"
 
-pass db '1234',0
-user db"rf",0
+pass1 db '1234',0
+user1 db"rez1",0
+pass2 db '4321',0
+user2 db"fatemeh",0
 pass_temp times 100 db 0
 user_temp  times 100 db 0
 m_user db "Enter user name: ",0
