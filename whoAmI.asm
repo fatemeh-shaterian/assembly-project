33af547039a400c236b2a9ea595f278c2e7e2faf
fatemeh-shaterian <fatemehshaterian@gmail.com>
2015-01-12 23:40:47 +0330

    Update whoAmI.asm

--- whoAmI.asm
+++ whoAmI.asm
@@ -3,13 +3,29 @@ who:
         call printString
         mov si,user_temp
         call printString
-        mov si,tab
-        call printString
+        mov byte[buffer],0dh
+        call printChar
+        mov byte[buffer],0ah
+        call printChar
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        ;add date
+ 	mov si,sign
+        call printString
+        mov si , dat
+        call printString
+	 mov si,tab
+        call printString
+        call getDate
+        mov si , sign
+        call printString
+        mov si , tim
+        call printString
+         mov si,tab
+        call printString
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;        
         mov ax , 0		
 	call getTime
 ret        
 
-tab db "                    ",0
+tab db "                 ",0
+dat db "current date :" , 0
+tim db "current time :" , 0
