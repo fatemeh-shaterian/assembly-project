992407ff2fcc5d48382a13c04f7aaa5bed66b9a5
fatemeh-shaterian <fatemehshaterian@gmail.com>
2015-01-12 23:19:27 +0330

    Update clearArray.asm

--- clearArray.asm
+++ clearArray.asm
@@ -1,11 +1,18 @@
+;--------------------
+; recieves : si = points to array we want to clean
+; returns : nothing
+;--------------------
+
 clearArray:
 pusha
-mov cx,100
-mov si,array
 
 .l1:
 mov byte[si],0
 inc si
+cmp byte [si] , 0
+je .out
 loop .l1
+
+.out:
 popa
 ret
