c24d0a97cdb0468690a8fb43beae1a950697fd93
fatemeh-shaterian <fatemehshaterian@gmail.com>
2015-01-12 23:42:53 +0330

    Update console_io.asm

--- console_io.asm
+++ console_io.asm
@@ -7,11 +7,12 @@
 ;;recieved: si-> point to array
 ;;pre condition:not
 ;;print string that its address is in si
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 printString:
 
     pusha
     mov ah, 0Eh
+    
 .loop1:
     lodsb
     cmp al, 0
@@ -59,22 +60,28 @@ printString:
 ;;recieved: buffer
 ;;pre condition:not
 ;;print char that is in buffer
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 printChar:
+        
         pusha
+        ;mov si,herr
+        ;call printString
         mov si , buffer
         mov ah,0Eh
         lodsb
+        ;mov cx , 01h
+        ;mov bl , 04h
         int 10h
         popa
         ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;return:not
-;;recieved: di-bx-> point to array
+;;recieved: di-bx-> point to array	dl = 1 if password is going to be entered  , 0 if not
 ;;pre condition:
 ;;get stirng and show it on screen untill enter
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 getString:
+
         pusha
         mov ax,0
         mov si, buffer
@@ -83,6 +90,18 @@ getString:
         l1:
         mov ah,10h
         int 16h
+        
+        cmp al , 224
+        jne .continue 
+        
+        call upper
+        
+        mov ah,10h
+        int 16h
+        jmp toend
+        
+        
+        .continue:
           
         cmp al,8
         jne .here
@@ -102,13 +121,30 @@ getString:
        cmp al,13
         je printStr
         
+        cmp al,9
+        jne .down
+        ;pusha
+        call tabFunc
+        ;popa
+        jmp .here1     
+        .down:
+        
+        push ax
+        cmp dl , 1
+        jne .d
+        mov al , '*'
+        
+        .d:
         mov byte[buffer] , al
+        pop ax
         mov byte[di],al
         
         call printChar
+        .here1:
         inc di
         jmp l1
-                    
+        
+                
 
         printStr:
          
@@ -118,21 +154,27 @@ getString:
         ;inc di
         mov byte[di] ,0         ; bayad bashe bara moghayese
         
+         toend:  
+        
         mov byte[buffer],0dh
         call printChar
         mov byte[buffer],0ah
         call printChar
         
-        ;to show what is seved in array
-        ;mov si,sign
+        
+        ;to show what is saved in array
+        mov si,sign
+        call printString   
+        ;mov si, array
         ;call printString   
-        ;mov si, bx
-        ;call printString   
-;---------------------------------------------------------------------
+;--------------------------------------------------------------------
+       
         mov byte[buffer],0dh
         call printChar
         mov byte[buffer],0ah
         call printChar
+        
+      
         popa
         ret
          
@@ -140,6 +182,7 @@ getString:
 ;sign db '>>> ',0
 buffer db 0
 enterStr db 0ah,0dh,0
+herr db "tab",0
 ; Write the rest of functions you need for console_io here
 ; you may also create other ".asm" files for rest of functions you need
 ; just do NOT forget to include them in "kernel.asm"
